98cdf7989fcd871a456fb5b284f762737313aaa0
laoshanxi <50568160+laoshanxi@users.noreply.github.com>
2019-05-30 15:08:09 +0800

    Update dockersave.py

--- dockersave.py
+++ dockersave.py
@@ -7,29 +7,27 @@ if __name__ == "__main__":
     os.system('rm -rf /tmp/saved-docker-images')
     os.system('mkdir -p /tmp/saved-docker-images')
     p = subprocess.Popen('docker images', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p.stdout.readline()
     for line in p.stdout.readlines():
-
-        # 此处的正则表达式是为了匹配镜像名以ufleet为开头的镜像
-        # 实际使用中根据需要自行调整
-        m = re.match(r'(^ufleet[^\s]*\s*)\s([^\s]*\s)', line.decode("utf-8"))
-        if not m:
+        m= re.split(r'\s+', line)
+        if len(m) > 2:
             continue
 
-        # 镜像名
-        iname = m.group(1).strip(' ')
-        # tag
-        itag = m.group(2)
+        # image name
+        iname = m[0]
+        # tag name
+        itag = m[1]
 
-        # tar包的名字
+        # tar package name
         tarname = iname.split('/')[-1]
         print(tarname)
         tarball = tarname + '.tar'
         ifull = iname + ':' + itag
         #save
         cmd = 'docker save -o ' + tarball + ' ' + ifull
+        print(cmd)
         os.system(cmd)
 
-        # 将tar包放在临时目录
         os.system('mv %s /tmp/saved-docker-images/'%tarball)
 
 
